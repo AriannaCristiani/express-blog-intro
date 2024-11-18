5e483876c57d16fe3d771c7cf3c79418af2430e2
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-18 15:07:11 +0100

    impostazione rotta con testo semplice

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
+app.get ('/', (req, res) => {
+    res.send('Server del mio Blog')
+
+})
+
 app.listen (port, () => {
     console.log(`il server è in ascolto sulla porta: ${port}`)
 })
